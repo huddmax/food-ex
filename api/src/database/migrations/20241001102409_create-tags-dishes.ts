72620ae4d9a628cd7204523239ba4314f097bd20
Hudson Machado <huddmax@gmail.com>
2024-10-01 17:43:54 -0300

    query de consulta ao database

--- api/src/database/migrations/20241001102409_create-tags-dishes.ts
+++ api/src/database/migrations/20241001102409_create-tags-dishes.ts
@@ -2,6 +2,12 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("tags", (table) => {
+        table.increments("id").primary(),
+        table.text("name").notNullable(),
+            
+        table.integer("dish_id").references("id").inTable("dishes").notNullable().onDelete("CASCADE");
+    })
 }
 
 
--- api/src/server.ts
+++ api/src/server.ts
@@ -51,12 +51,67 @@ app.delete("/dishes/:id", async (request: Request, response: Response) => {
  })
 
 
+// app.post("/tags", async (request: Request, response: Response) => {
+//     try {
+//         const { dish_id, name } = request.body;
+        
+//         if (!dish_id || !name) {
+            
+//         return response.status(400).json({ error: 'dish_id and name are required' });
+//       }
+      
+//       await knex('tags').insert({ dish_id, name });
+//       return response.status(201).json({ message: 'Tag created successfully' });
+//     }
+    
+//     catch (error) {
+//       console.error(error);
+//       return response.status(500).json({ error: 'Internal Server Error' });
+//     }
+// });
+
+app.post("/tags", async (request: Request, response: Response) => {
+    const { name, dish_id } = request.body;
+    
+    await knex("tags").insert({ name, dish_id });
+    
+    return response.status(201).json();
+    
+  });
+  
+
+app.get("/tags", async (request: Request, response: Response) => {
+    const tags = await knex("tags").select().orderBy("name");
+
+    return response.status(200).json(tags);
+});
+
+app.get("/dishes/:id/tags", async (request: Request, response: Response) => {
+
+    const dishes = await knex("dishes")
+        .select(
+            "tags.id AS tag_id",
+            "tags.name AS tag",
+            "dishes.name AS dish",
+            "dishes.price",
+            "dishes.id AS dish_id",
+
+        )
+        .join("tags", "dishes.id", "tags.dish_id");
+
+    return response.json(dishes);
+});
+
+
+
 
 /*
     Tipos de Erro:
     400 (Bad Request): Erro do cliente.
     500 (Internal Server Error): Erro interno do servidor.
 */
+
+
 
 // esse tem que ficar abaxio de todos e logo acima do listen, para poder capturar os erros
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
