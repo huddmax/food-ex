68cd54a2156fd6c56a02c7a09a1be7b0c1100e5c
Hudson Machado <huddmax@gmail.com>
2024-10-23 19:53:48 -0300

    Improve UsersController by adding name validation and preventing duplicate admin users

--- api/src/controllers/UsersController.ts
+++ api/src/controllers/UsersController.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { knex } from "../database/knex";
 import { z } from "zod";
 import { AppError } from "@/utils/AppError";
+import bcrypt from "bcrypt";
 
 class UsersController {
     async create(request: Request, response: Response, next: NextFunction) { 
@@ -14,19 +15,25 @@ class UsersController {
             
             const { name, email, password } = bodySchema.parse(request.body);
 
+            if (name.toLocaleLowerCase() === "admin") {
+                throw new AppError("Invalid user name", 400);
+             }
+
+
             const existingUser = await knex<UsersRepository>('users').where({ email }).first();
              
             if (existingUser) { 
                 throw new AppError("User already exists", 409);
             };
 
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+
             await knex<UsersRepository>("users").insert({
                 name,
                 email,
-                password,
+                password: hashedPassword,
             });
-
-
             return response.status(201).json({ message: "User created" });
         } catch (error) {
             next(error);
