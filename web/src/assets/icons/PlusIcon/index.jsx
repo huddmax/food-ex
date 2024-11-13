93f41fd70bc70865d42a8af04e3ef963d89880be
Hudson Machado <41968106+huddmax@users.noreply.github.com>
2024-11-13 20:01:17 -0300

    Delete web directory

--- web/src/assets/icons/PlusIcon/index.jsx
+++
@@ -1,8 +0,0 @@
-export function PlusIcon() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
-  <path fillRule="evenodd" clipRule="evenodd" d="M0 4C0 3.81591 0.149238 3.66667 0.333333 3.66667H7.66667C7.85076 3.66667 8 3.81591 8 4C8 4.18409 7.85076 4.33333 7.66667 4.33333H0.333333C0.149238 4.33333 0 4.18409 0 4Z" fill="#7C7C8A"/>
-  <path fillRule="evenodd" clipRule="evenodd" d="M4 0C4.18409 0 4.33333 0.149238 4.33333 0.333333V7.66667C4.33333 7.85076 4.18409 8 4 8C3.81591 8 3.66667 7.85076 3.66667 7.66667V0.333333C3.66667 0.149238 3.81591 0 4 0Z" fill="#7C7C8A"/>
-</svg>
-    )
-}
